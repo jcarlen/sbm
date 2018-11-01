e37edc93021bf84cecc08b42983fe1914dd17796
Jane Carlen <janecarlen@adminisatorsMBP.attlocal.net>
2018-10-31 17:45:00 -0700

    Handle non 0/1 degreeCorrect variable

--- sbm/R/sbm.R
+++ sbm/R/sbm.R
@@ -46,7 +46,7 @@ sbm <- function(edgelist, maxComms = 2, degreeCorrect = F, directed = FALSE, klP
         stop("Directed should be FALSE or TRUE (0 or 1 OK)")
     }
     
-    degreeCorrect = as.numeric(degreeCorrect) # relic of Karrer and Newman code to have degreeCorrect be 0|1
+    degreeCorrect = as.numeric(!degreeCorrect==0) # relic of Karrer and Newman code to have degreeCorrect be 0|1
     
     #seed?
     
