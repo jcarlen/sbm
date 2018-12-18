7786fabd597e76e97929177fe7ab4b2bb011ed6f
Jane Carlen <janecarlen@adminisatorsMBP.attlocal.net>
2018-12-17 22:39:20 -0800

    adding time-dependent tests

--- sbm/tests/testthat/test_time_dependent.R
+++ sbm/tests/testthat/test_time_dependent.R
@@ -1,7 +1,31 @@
-#!/bin/sh
+library(sbm)
+data(la_byhour_edgelist)
 
-#  test_time_dependent.R
-#  
-#
-#  Created by Jane Carlen on 12/17/18.
-#  
+test_that("sbm doesn't fail on uncorrected, weighted, time-dependent network", {
+    
+    set.seed(1)
+    #undirected and directed - no degree correction
+    expect_equal(
+        5,
+        length( sbmt(la_byhour_edgelist,  degreeCorrect = F, directed = F, klPerNetwork = 5, maxComms = 2))
+    )
+    
+    expect_equal(
+        5,
+        length( sbmt(la_byhour_edgelist,  degreeCorrect = F, directed = T, klPerNetwork = 5, maxComms = 2))
+    )
+})
+
+test_that("sbm doesn't fail on corrected, undirected, weighted, time-dependent network", {
+    
+    #undirected and directed - degree correction level 1
+    expect_equal(
+        5,
+        length(sbmt(la_byhour_edgelist,  degreeCorrect = 1, directed = F, klPerNetwork = 5, maxComms = 2))
+    )
+    
+    expect_equal(
+        5,
+        length(sbmt(la_byhour_edgelist,  degreeCorrect = T, directed = T, klPerNetwork = 5, maxComms = 2))
+    )
+})
