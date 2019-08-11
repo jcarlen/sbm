677f800b959b6674953b2fbff16d44b4ba2cd0c4
jcarlen <jane.carlen@gmail.com>
2019-08-11 10:50:12 -0700

    new tests to make sure time-dependent and not time-dependent code return same results when T = 1

---
+++ sbmt/tests/testthat/test_sbm_vs_sbmt.R
@@ -0,0 +1,12 @@
+library(sbmt)
+data(zach)
+
+test_that("test time-dependent and not time-dependent code gives same results when T = 1", {
+  expect_equal(sbm(zach, seed = 1)$HighestScore, sbmt(list(zach), seed = 1)$HighestScore)
+  expect_equal(sbm(zach, directed = T, seed = 1)$HighestScore, sbmt(list(zach), directed = T, seed = 1)$HighestScore)
+  expect_equal(sbm(zach, degreeCorrect = 1, seed = 1)$HighestScore,  sbmt(list(zach), degreeCorrect = 1, seed = 1)$HighestScore)
+  expect_equal(sbm(zach, directed = T, degreeCorrect = 1, seed = 1)$HighestScore, 
+               sbmt(list(zach), directed = T, degreeCorrect = 1, seed = 1)$HighestScore)
+  expect_equal(sbm(zach, directed = T, degreeCorrect = 1, seed = 1)$HighestScore, 
+               sbmt(list(zach), directed = T, degreeCorrect = 2, seed = 1)$HighestScore)
+})
