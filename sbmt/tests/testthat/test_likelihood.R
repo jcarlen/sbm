7611eb555ac52f7164d1e835579492a999818420
jane.carlen <jane.carlen@latimes.com>
2020-09-17 11:24:42 -0400

    T to TRUE

--- sbmt/tests/testthat/test_likelihood.R
+++ sbmt/tests/testthat/test_likelihood.R
@@ -9,7 +9,7 @@ test_that("edgelist to adjacency, as.array=TRUE, undireced", {
     #selfEdges FALSE ----
   
       #on diagonal 
-  expect_equal(edgelist_to_adj(la_byhour_edgelist, selfEdges = FALSE, as.array = T, directed = FALSE)[9,9,],
+  expect_equal(edgelist_to_adj(la_byhour_edgelist, selfEdges = FALSE, as.array = TRUE, directed = FALSE)[9,9,],
                setNames(rep(0,24), as.character(1:24)))
   
       #off diagonal
@@ -21,13 +21,13 @@ test_that("edgelist to adjacency, as.array=TRUE, undireced", {
     #selfEdges TRUE ----
       #on diagonal
   
-  expect_equal(edgelist_to_adj(la_byhour_edgelist, selfEdges = T, as.array = T, directed = FALSE)[9,9,],
+  expect_equal(edgelist_to_adj(la_byhour_edgelist, selfEdges = TRUE, as.array = TRUE, directed = FALSE)[9,9,],
                setNames(sapply(la_byhour_edgelist, function(x) {
                  sum(x[,3][(x[,1]=="3038" & x[,2]=="3038")])
                  }), as.character(1:24)))
        
      #off diagonal        
-  expect_equal(edgelist_to_adj(la_byhour_edgelist, selfEdges = T, as.array = T, directed = FALSE)[1,2,],
+  expect_equal(edgelist_to_adj(la_byhour_edgelist, selfEdges = TRUE, as.array = TRUE, directed = FALSE)[1,2,],
                setNames(sapply(la_byhour_edgelist, function(x) {
                  sum(x[,3][(x[,1]=="3008" & x[,2]=="3031") | (x[,1]=="3031" & x[,2]=="3008")])
                }), as.character(1:24)))
@@ -40,7 +40,7 @@ test_that("edgelist to adjacency, as.array=TRUE, directed", {
     #selfEdges FALSE ----
   
     #on diagonal 
-  expect_equal(edgelist_to_adj(la_byhour_edgelist, selfEdges = FALSE, as.array = T, directed = TRUE)[9,9,],
+  expect_equal(edgelist_to_adj(la_byhour_edgelist, selfEdges = FALSE, as.array = TRUE, directed = TRUE)[9,9,],
                setNames(rep(0,24), as.character(1:24)))
   
     #off diagonal
@@ -156,33 +156,34 @@ test_that("tdd_sbm_llik - likelihood for discrete model", {
 # from initialization to final (note reported InitialScore gives varying levels of accuracy)
 
 test_that("Likelihood accounting, T = 1", {
-  A = edgelist_to_adj(list(zach), directed = T)
-  model1 = sbmt(list(zach), directed= T, degreeCorrect = 3, klPerNetwork = 1, seed = 1)
+  A = edgelist_to_adj(list(zach), directed = TRUE)
+  model1 = sbmt(list(zach), directed= TRUE, degreeCorrect = 3, klPerNetwork = 1, seed = 1)
   init =  rep_len(c(0,1), length(model1$FoundComms)); names(init) = names(model1$FoundComms)
   
   tmp = data.frame(zach, g1 = init[as.character(zach[,1])], g2 = init[as.character(zach[,2])])
   init.EdgeMatrix = matrix(aggregate(rep(1, nrow(tmp)), by = list(tmp$g1,tmp$g2), sum)$x, nrow = 2, ncol = 2)
   
-  model.init = sbmt(list(zach), directed= T, degreeCorrect = 3, klPerNetwork = 1, seedComms = init, seed = 1)
+  model.init = sbmt(list(zach), directed = TRUE, degreeCorrect = 3, klPerNetwork = 1, seedComms = init, seed = 1)
+  
   #These should be approximately equal:
   x1 = model.init$HighestScore - (-447.463) #InitialScore
-  x2 = tdd_sbm_llik(A, model.init$FoundComms, model.init$EdgeMatrix, directed = T) - tdd_sbm_llik(A, init, init.EdgeMatrix, directed = T)
+  x2 = tdd_sbm_llik(A, model.init$FoundComms, model.init$EdgeMatrix, directed = TRUE) - tdd_sbm_llik(A, init, init.EdgeMatrix, directed = TRUE)
   expect_true(abs(x1 - x2) < .001)
 })
 
 test_that("Likelihood accounting, T = 24", {
-  A = edgelist_to_adj(la_byhour_edgelist, directed = T)
-  model2 = sbmt(la_byhour_edgelist, directed= T, degreeCorrect = 3, klPerNetwork = 1, seed = 1)
+  A = edgelist_to_adj(la_byhour_edgelist, directed = TRUE)
+  model2 = sbmt(la_byhour_edgelist, directed= TRUE, degreeCorrect = 3, klPerNetwork = 1, seed = 1)
   init2 =  rep_len(c(0,1), length(model2$FoundComms)); names(init2) = names(model2$FoundComms)
   init.EdgeMatrix = lapply(la_byhour_edgelist, function(data.t) {
     tmp = data.frame(data.t, g1 = init2[as.character(data.t[,1])], g2 = init2[as.character(data.t[,2])])
     tmp2 = aggregate(data.t$x, by = list(tmp$g1,tmp$g2), sum)$x
     init.EdgeMatrix = matrix(tmp2, nrow = 2, ncol = 2)
   })
-  model.init = sbmt(la_byhour_edgelist, directed= T, degreeCorrect = 3, klPerNetwork = 1, seedComms = init2, seed = 1)
+  model.init = sbmt(la_byhour_edgelist, directed= TRUE, degreeCorrect = 3, klPerNetwork = 1, seedComms = init2, seed = 1)
   #These should be approximately equal:
   x1 = model.init$HighestScore - (-429267) #InitialScore - note it's rounded, so less precision below
-  x2 = tdd_sbm_llik(A, model.init$FoundComms, model.init$EdgeMatrix, directed = T) -  tdd_sbm_llik(A, init2, init.EdgeMatrix, directed = T)
+  x2 = tdd_sbm_llik(A, model.init$FoundComms, model.init$EdgeMatrix, directed = TRUE) -  tdd_sbm_llik(A, init2, init.EdgeMatrix, directed = TRUE)
   expect_true(abs(x1 - x2) < 1)
 })
 
